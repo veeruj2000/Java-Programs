d410a172fc7716b098a4b3483e5533bdd6a53068
UJWAL PANKAJ JAIN <57255611+veeruj2000@users.noreply.github.com>
2021-11-03 16:49:47 +0530

    Hello World
    In this code, the ouput displays Hello World. This code was made for submission in MyCaptain Exercise.

---
+++ Hello World MyCaptain/README.md
@@ -0,0 +1,2 @@
+# project1_java_mycaptain
+This program describes about printing a Hello World to give us a basic understanding about the Java Syntax and other basic components.
---
+++ Hello World MyCaptain/project1_mycaptain.java
@@ -0,0 +1,7 @@
+package com.company.mycaptain;
+
+public class project1_mycaptain {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
